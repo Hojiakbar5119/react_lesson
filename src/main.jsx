36d2e9e4632c1c8b372e7900674598e5d21756d8
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-21 17:09:33 +0500

    first homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Fruit from './furit';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Fruit/>
+    {/* <Fruit /> */}
   </React.StrictMode>
 );
