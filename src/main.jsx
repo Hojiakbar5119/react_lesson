17691c50255da5a1ea33355155591c143b92a359
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-23 18:41:19 +0500

    lesson 3

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Student from './Lesson2/student';
+import FunctionCall from './Lesson3/functionCall';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Fruit /> */}
-    <Student/>  
+    {/* <Student/>   */}
+    <FunctionCall/>
   </React.StrictMode>
 );
