232ccf4102ea2cb822960a348306656668cac6b2
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-31 22:43:50 +0500

    lesson5

---
+++ src/Lesson5/learnComponents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Components } from "./style";
+class LearnComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Shunaqa</p>
+                <h1>Learning react components</h1>
+            </div>
+        )
+    }
+}
+export default LearnComponent
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import Student from './Lesson2/student';
 import FunctionCall from './Lesson3/functionCall';
 import Crud from './Lesson4/crud';
+import LearnComponent from './Lesson5/learnComponents';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Fruit /> */}
     {/* <Student/>   */}
     {/* <FunctionCall/> */}
-    <Crud/>
+    {/* <Crud/> */}
+    <LearnComponent/>
   </React.StrictMode>
 );
