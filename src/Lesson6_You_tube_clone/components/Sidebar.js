0d23799fd6a50dbe00a47a1efc191b77bc2e6e19
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-05 07:37:16 +0500

    change image

---
+++ src/Lesson6_You_tube_clone/components/Sidebar.js
@@ -0,0 +1,29 @@
+import { ReactComponent as home } from "../assets/logo/home.svg";
+
+export const sidebarlogo = [
+  {
+    id: 1,
+    data: [
+      { id: 1, title: "Home", Icon: home },
+      { id: 2, title: "Explore", Icon: explore },
+      { id: 3, title: "Subscriptions", Icon: subscriptions },
+    ],
+  },
+  {
+    id: 2,
+    data: [
+      { id: 1, title: "Library", Icon: library },
+      { id: 2, title: "History", Icon: history },
+      { id: 3, title: "Explore", Icon: explore },
+    ],
+  },
+  {
+    id: 3,
+    title:'Subscription',
+    data: [
+      { id: 1, title: "Home", Icon: home },
+      { id: 2, title: "Explore", Icon: explore },
+      { id: 3, title: "Explore", Icon: explore },
+    ],
+  },
+];
