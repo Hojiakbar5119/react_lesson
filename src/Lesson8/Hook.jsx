359503a823931fb09432f654dfcd261fcf2bfba8
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-14 21:05:43 +0500

    usereducer

--- src/Lesson8/Hook.jsx
+++ src/Lesson8/Hook.jsx
@@ -1,21 +1,65 @@
-import React, { useState } from "react";
-import { Textbutton } from "./style";
-// import './style.js'
-const Hook = (props) => {
-  const [count, setCount] = useState(10);
-  // console.log(state);
-  const [name, SetName] = useState("Webbrain");
-  console.log(SetName);
+// ?  this component yordamida ishladik
+
+// import React, { Component } from "react";
+
+// export default class Hook extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 1,
+//     };
+//   }
+//   render() {
+//     // const plus=()=>{
+//     //   this.setState({count: this.state.count+1})
+//     //   console.log(this.state.count);
+//     // }
+//     return (
+//       <div>
+//         <h1>Hook</h1>
+//         <h3>Count = {this.state.count} </h3>
+//         <button
+//           onClick={() => {
+//             this.setState({ count: this.state.count + 1 });
+//           }}
+//         >
+//           increment
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useState, useReducer } from "react";
+
+const Hook = () => {
+  const [count, setCount] = useState(0);
+  const [counter, dispatch] = useReducer((state, action) => {
+    console.log(state, action);
+
+    switch (action.type) {
+      case "plus":
+        return state + 1;
+      case "minus":
+        return state - 1;
+      case "doubleplus":
+        return state + action.payload;
+      default:
+        state;
+    }
+  },0);
+  // console.log(count, setCount);
   return (
     <div>
-      <h1>Hooks</h1>
-      <p>Counts {count} </p>
-      <Textbutton onClick={() => setCount(count + 1)}>+</Textbutton>
-
-      <p>Name: {name}</p>
-      <input type="text" value={name} onChange={({target})=>SetName(target.value)} />
+      <h1>Hook</h1>
+      <h2>Count {count} </h2>
+      <h3>Counter : {counter} </h3>
+      <button onClick={() => dispatch({ type: "plus" })}>plus</button>
+      <button onClick={() => dispatch({ type: "minus" })}>minus</button>
+      <button onClick={() => dispatch({ type: "doubleplus", payload:5 })}>5</button>
+      <hr />
+      <button onClick={() => setCount(count + 1)}>incrememnt</button>
     </div>
   );
 };
-
 export default Hook;
