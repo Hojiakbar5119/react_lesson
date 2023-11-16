e32756ceaec0cdf685bbf1d8487ccc4fc7e4f56d
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-16 19:28:52 +0500

    fetch

--- src/Lesson11/Fetch.jsx
+++ src/Lesson11/Fetch.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 const Fetch = () => {
   const [student, setStudents] = useState([]);
-
+  const [select, setSelected] = useState([]);
+  
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users", { method: "Get" })
       .then((res) => res.json())
       .then((res) => {
         console.log(res);
@@ -11,16 +12,39 @@ const Fetch = () => {
       });
   }, []);
 
+  const getinfo=(id)=>{
+    console.log(id);
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`, { method: "Get" })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setSelected(res);
+      });
+
+  }
   return (
     <div>
       <h1>Students list</h1>
       <p>Talabalar ro'yxati </p>
 
-    <ul>
-    {student.map((value)=>{
-        return <li key={value.id}>{value.id}. {value.name}</li>
-      })}
-    </ul>
+      <ul>
+        {student.map((value) => {
+          return (
+            <li key={value.id}>
+              {value.id}. {value.name}
+              <button onClick={()=>{getinfo(value.id)}}>get Info</button>
+            </li>
+          );
+        })}
+      </ul>
+     <div key={select.id}>
+     <h3>
+        Qo'shimcha malumotlar
+      </h3>
+      <p>Username: {select.username} </p>
+      <p>Adress: {select.id} </p>
+     </div>
 
       {/* <button onClick={()=>{setCounter(+1)}}>Plus degrees</button> */}
     </div>
